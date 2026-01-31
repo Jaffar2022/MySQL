081892cb2873f8c41db4b19c7069a27086672e64
Jaffar2022 <103681678+Jaffar2022@users.noreply.github.com>
2026-01-31 11:39:36 +0530

    Add files via upload

---
+++ Cross Join.sql
@@ -0,0 +1,5 @@
+Create table drink_new (drinkname varchar(20), rate double);
+insert into drink_new values ("Tea",5.00),("Coffee",15.00),("Pepsi",20.00);
+
+Create table Meals_new (mealname varchar(20), rate double);
+insert into Meals_new values ("Omlet",10.50),("Sandwich",15.50),("Pancake",40.75);
---
+++ Joins Table-1 to Table-3.sql
@@ -0,0 +1,115 @@
+CREATE TABLE Table_01 (
+    Name	VARCHAR(512),
+    Score	INT
+);
+
+INSERT INTO Table_01 (Name, Score) VALUES ('raj', '31');
+INSERT INTO Table_01 (Name, Score) VALUES ('kumar', '78');
+INSERT INTO Table_01 (Name, Score) VALUES ('rao', '65');
+INSERT INTO Table_01 (Name, Score) VALUES ('mohan', '26');
+INSERT INTO Table_01 (Name, Score) VALUES ('prasad', '24');
+INSERT INTO Table_01 (Name, Score) VALUES ('daya', '75');
+INSERT INTO Table_01 (Name, Score) VALUES ('kiran', '61');
+INSERT INTO Table_01 (Name, Score) VALUES ('rajesh', '76');
+INSERT INTO Table_01 (Name, Score) VALUES ('suresh', '80');
+
+
+CREATE TABLE Table_02 (
+    Name	VARCHAR(512),
+    Mobile_number	bigint,
+    City	VARCHAR(512)
+);
+
+INSERT INTO Table_02 (Name, Mobile_number, City) VALUES ('raj', 8461269292, 'Pune');
+INSERT INTO Table_02 (Name, Mobile_number, City) VALUES ('kumar', 8280104397, 'del');
+INSERT INTO Table_02 (Name, Mobile_number, City) VALUES ('mohan', 8578417896, 'mum');
+INSERT INTO Table_02 (Name, Mobile_number, City) VALUES ('daya', 9236769743, 'pune');
+INSERT INTO Table_02 (Name, Mobile_number, City) VALUES ('srinu', 8837622677, 'Mum');
+INSERT INTO Table_02 (Name, Mobile_number, City) VALUES ('wasim', 7918929179, 'Chi');
+
+
+CREATE TABLE Table_03 (
+    Name	VARCHAR(512),
+    Score	INT
+);
+
+INSERT INTO Table_03 (Name, Score) VALUES ('raj', '50');
+INSERT INTO Table_03 (Name, Score) VALUES ('kumar', '35');
+INSERT INTO Table_03 (Name, Score) VALUES ('rao', '83');
+INSERT INTO Table_03 (Name, Score) VALUES ('mohan', '18');
+INSERT INTO Table_03 (Name, Score) VALUES ('prasad', '82');
+INSERT INTO Table_03 (Name, Score) VALUES ('daya', '42');
+INSERT INTO Table_03 (Name, Score) VALUES ('kiran', '20');
+INSERT INTO Table_03 (Name, Score) VALUES ('rajesh', '44');
+INSERT INTO Table_03 (Name, Score) VALUES ('suresh', '73');
+INSERT INTO Table_03 (Name, Score) VALUES ('raj', '80');
+INSERT INTO Table_03 (Name, Score) VALUES ('kumar', '66');
+INSERT INTO Table_03 (Name, Score) VALUES ('rao', '15');
+INSERT INTO Table_03 (Name, Score) VALUES ('mohan', '61');
+INSERT INTO Table_03 (Name, Score) VALUES ('prasad', '88');
+INSERT INTO Table_03 (Name, Score) VALUES ('daya', '98');
+INSERT INTO Table_03 (Name, Score) VALUES ('kiran', '90');
+INSERT INTO Table_03 (Name, Score) VALUES ('rajesh', '28');
+INSERT INTO Table_03 (Name, Score) VALUES ('suresh', '35');
+INSERT INTO Table_03 (Name, Score) VALUES ('raj', '34');
+INSERT INTO Table_03 (Name, Score) VALUES ('kumar', '88');
+INSERT INTO Table_03 (Name, Score) VALUES ('rao', '2');
+INSERT INTO Table_03 (Name, Score) VALUES ('mohan', '73');
+INSERT INTO Table_03 (Name, Score) VALUES ('prasad', '71');
+INSERT INTO Table_03 (Name, Score) VALUES ('daya', '17');
+INSERT INTO Table_03 (Name, Score) VALUES ('kiran', '95');
+INSERT INTO Table_03 (Name, Score) VALUES ('rajesh', '62');
+INSERT INTO Table_03 (Name, Score) VALUES ('suresh', '4');
+INSERT INTO Table_03 (Name, Score) VALUES ('raj', '48');
+INSERT INTO Table_03 (Name, Score) VALUES ('kumar', '42');
+INSERT INTO Table_03 (Name, Score) VALUES ('rao', '48');
+INSERT INTO Table_03 (Name, Score) VALUES ('mohan', '31');
+INSERT INTO Table_03 (Name, Score) VALUES ('prasad', '54');
+INSERT INTO Table_03 (Name, Score) VALUES ('daya', '79');
+INSERT INTO Table_03 (Name, Score) VALUES ('kiran', '84');
+INSERT INTO Table_03 (Name, Score) VALUES ('rajesh', '65');
+INSERT INTO Table_03 (Name, Score) VALUES ('suresh', '46');
+INSERT INTO Table_03 (Name, Score) VALUES ('raj', '40');
+INSERT INTO Table_03 (Name, Score) VALUES ('kumar', '59');
+INSERT INTO Table_03 (Name, Score) VALUES ('rao', '74');
+INSERT INTO Table_03 (Name, Score) VALUES ('mohan', '29');
+INSERT INTO Table_03 (Name, Score) VALUES ('prasad', '15');
+INSERT INTO Table_03 (Name, Score) VALUES ('daya', '43');
+INSERT INTO Table_03 (Name, Score) VALUES ('kiran', '37');
+INSERT INTO Table_03 (Name, Score) VALUES ('rajesh', '21');
+INSERT INTO Table_03 (Name, Score) VALUES ('suresh', '61');
+INSERT INTO Table_03 (Name, Score) VALUES ('raj', '76');
+INSERT INTO Table_03 (Name, Score) VALUES ('kumar', '25');
+INSERT INTO Table_03 (Name, Score) VALUES ('rao', '17');
+INSERT INTO Table_03 (Name, Score) VALUES ('mohan', '35');
+INSERT INTO Table_03 (Name, Score) VALUES ('prasad', '72');
+INSERT INTO Table_03 (Name, Score) VALUES ('daya', '50');
+INSERT INTO Table_03 (Name, Score) VALUES ('kiran', '89');
+INSERT INTO Table_03 (Name, Score) VALUES ('rajesh', '8');
+INSERT INTO Table_03 (Name, Score) VALUES ('suresh', '59');
+INSERT INTO Table_03 (Name, Score) VALUES ('raj', '24');
+INSERT INTO Table_03 (Name, Score) VALUES ('kumar', '13');
+INSERT INTO Table_03 (Name, Score) VALUES ('rao', '43');
+INSERT INTO Table_03 (Name, Score) VALUES ('mohan', '1');
+INSERT INTO Table_03 (Name, Score) VALUES ('prasad', '2');
+INSERT INTO Table_03 (Name, Score) VALUES ('daya', '38');
+INSERT INTO Table_03 (Name, Score) VALUES ('kiran', '79');
+INSERT INTO Table_03 (Name, Score) VALUES ('rajesh', '39');
+INSERT INTO Table_03 (Name, Score) VALUES ('suresh', '55');
+INSERT INTO Table_03 (Name, Score) VALUES ('raj', '29');
+INSERT INTO Table_03 (Name, Score) VALUES ('kumar', '54');
+INSERT INTO Table_03 (Name, Score) VALUES ('rao', '59');
+INSERT INTO Table_03 (Name, Score) VALUES ('mohan', '63');
+INSERT INTO Table_03 (Name, Score) VALUES ('prasad', '3');
+INSERT INTO Table_03 (Name, Score) VALUES ('daya', '23');
+INSERT INTO Table_03 (Name, Score) VALUES ('kiran', '30');
+INSERT INTO Table_03 (Name, Score) VALUES ('rajesh', '19');
+INSERT INTO Table_03 (Name, Score) VALUES ('suresh', '38');
+INSERT INTO Table_03 (Name, Score) VALUES ('raj', '58');
+INSERT INTO Table_03 (Name, Score) VALUES ('kumar', '96');
+INSERT INTO Table_03 (Name, Score) VALUES ('rao', '67');
+INSERT INTO Table_03 (Name, Score) VALUES ('mohan', '96');
+INSERT INTO Table_03 (Name, Score) VALUES ('prasad', '63');
+INSERT INTO Table_03 (Name, Score) VALUES ('daya', '53');
+INSERT INTO Table_03 (Name, Score) VALUES ('kiran', '44');
+INSERT INTO Table_03 (Name, Score) VALUES ('rajesh', '30');
